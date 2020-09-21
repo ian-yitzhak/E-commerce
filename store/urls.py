3aa4ce9383da747a050cf94d3a569e1c03d2d36e
ian-yitzhak <onyangoian71@gmail.com>
2020-09-21 17:49:50 +0300

    first

---
+++ store/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path 
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+   
+
+    path('', views.product_list , name = 'product_list' ),
+
+
+    path(r'^(?P<category_slug>[-\w]+)/$',views.product_list,name='product_list_by_category'),
+
+    path(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',views.product_detail,name='product_detail'),
+
+
+]
